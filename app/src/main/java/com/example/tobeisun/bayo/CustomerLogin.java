b0fe503e8dfdb32bac1d755e9779494fb26c1904
t.obeisun <obeisuntabithaa@gmail.com>
2018-05-28 17:06:11 +0100

    no errors , driver and customer register and login, driver's map

--- app/src/main/java/com/example/tobeisun/bayo/CustomerLogin.java
+++ app/src/main/java/com/example/tobeisun/bayo/CustomerLogin.java
@@ -1,11 +1,23 @@
 package com.example.tobeisun.bayo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerLogin extends AppCompatActivity {
 
@@ -15,11 +27,20 @@ public class CustomerLogin extends AppCompatActivity {
     Button custregister;
     TextView custstatus;
     TextView custreglink ;
+    FirebaseAuth auth;
+ProgressDialog loadingbar ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
+
+        auth = FirebaseAuth.getInstance();
+
+loadingbar= new ProgressDialog(this);
+
+
 
 custemail= (EditText) findViewById(R.id.cust_email) ;
 custpassword=(EditText) findViewById(R.id.cust_password);
@@ -28,13 +49,16 @@ custregister=(Button) findViewById(R.id.cust_regbtn) ;
 custstatus=(TextView) findViewById(R.id.cust_status);
 custreglink=(TextView) findViewById(R.id.register_cust_link) ;
 
+
 custregister.setVisibility(View.INVISIBLE);
 custregister.setEnabled(false);
+
 
 
        custreglink.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+
                custlogin.setVisibility(View.INVISIBLE);
 
 
@@ -42,9 +66,127 @@ custregister.setEnabled(false);
                custreglink.setVisibility(View.INVISIBLE);
                custregister.setVisibility(View.VISIBLE);
                custregister.setEnabled(true);
+
+
+
+
+
+
+
            }
        });
 
 
+
+       custregister.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+
+               String email = custemail.getText().toString().trim();
+               String password = custpassword.getText().toString().trim();
+
+               Registercustomer(email,password);
+
+
+                       }
+                   });
+
+
+
+
+
+
+
+        custlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = custemail.getText().toString().trim();
+                String password = custpassword.getText().toString().trim();
+
+                Logincustomer (email,password);
+
+            }
+        });
+               }
+
+    private void Registercustomer(String email, String password)
+
+        {
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(CustomerLogin.this, "Please enter email", Toast.LENGTH_LONG).show();
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(CustomerLogin.this, "Please enter a valid password", Toast.LENGTH_LONG).show();
+            }
+            if(email.length() !=6 )
+            {
+                Toast.makeText(CustomerLogin.this,"Password must be 6 digits",Toast.LENGTH_LONG).show();
+            }
+
+
+
+            else {
+
+                loadingbar.setTitle("Customer registration");
+                loadingbar.setMessage("Please wait while we register your data ...");
+                loadingbar.show();
+
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CustomerLogin.this, "Succeful Signup", Toast.LENGTH_LONG).show();
+                            loadingbar.dismiss();
+                        } else {
+                            Toast.makeText(CustomerLogin.this, "Sign up failed , please try again later", Toast.LENGTH_LONG).show();
+                            loadingbar.dismiss();
+                        }
+
+                    }
+                });
+
+            }
+            }
+
+
+        private void Logincustomer (String email, String password){
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(CustomerLogin.this, "Please enter email", Toast.LENGTH_LONG).show();
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(CustomerLogin.this, "Please enter a valid password", Toast.LENGTH_LONG).show();
+            } else {
+
+                loadingbar.setTitle("Customer Login");
+                loadingbar.setMessage("Please wait  ...");
+                loadingbar.show();
+
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CustomerLogin.this, "Successful Login", Toast.LENGTH_LONG).show();
+                            loadingbar.dismiss();
+                        } else {
+                            Toast.makeText(CustomerLogin.this, "Login failed , please try again ", Toast.LENGTH_LONG).show();
+                            loadingbar.dismiss();
+                        }
+
+                    }
+                });
+
+
+            }
+
+
+        }
+
     }
-}
+
--- app/src/main/java/com/example/tobeisun/bayo/DriverLogin.java
+++ app/src/main/java/com/example/tobeisun/bayo/DriverLogin.java
@@ -1,31 +1,52 @@
 package com.example.tobeisun.bayo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class DriverLogin extends AppCompatActivity {
-   EditText Driveremail;
-    EditText Driverpassword ;
-    Button driverlogin ;
+    EditText Driveremail;
+    EditText Driverpassword;
+    Button driverlogin;
     Button driverregister;
     TextView driverstatus;
-    TextView driverreglink ;
+    TextView driverreglink;
+    FirebaseAuth auth;
+
+
+
+    ProgressDialog loadingbar ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login);
+        auth = FirebaseAuth.getInstance();
+        loadingbar= new ProgressDialog(this);
 
-        Driveremail= (EditText) findViewById(R.id.driver_email);
-        Driverpassword=(EditText) findViewById(R.id.driver_password);
-        driverlogin=(Button) findViewById(R.id.driver_loginbtn);
-        driverregister=(Button) findViewById(R.id.driver_registerbtn);
-        driverstatus=(TextView) findViewById(R.id.driver_status);
-        driverreglink=(TextView) findViewById(R.id.driver_reglink);
+        Driveremail = (EditText) findViewById(R.id.driver_email);
+        Driverpassword = (EditText) findViewById(R.id.driver_password);
+        driverlogin = (Button) findViewById(R.id.driver_loginbtn);
+        driverregister = (Button) findViewById(R.id.driver_registerbtn);
+        driverstatus = (TextView) findViewById(R.id.driver_status);
+        driverreglink = (TextView) findViewById(R.id.driver_reglink);
 
 
         driverregister.setVisibility(View.INVISIBLE);
@@ -45,5 +66,147 @@ public class DriverLogin extends AppCompatActivity {
             }
         });
 
+
+        driverregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String email = Driveremail.getText().toString().trim();
+                String password = Driverpassword.getText().toString().trim();
+
+                Registerdriver(email,password);
+
+
+            }
+        });
+
+
+        driverlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = Driveremail.getText().toString().trim();
+                String password = Driverpassword.getText().toString().trim();
+
+                LoginDriver (email,password);
+
+            }
+        });
+
+
+
+
+
+
     }
+
+    private void LoginDriver(String email, String password)
+    {
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(DriverLogin.this,"Please enter email",Toast.LENGTH_LONG).show();
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(DriverLogin.this,"Please enter a valid password",Toast.LENGTH_LONG).show();
+        }
+
+
+
+        else{
+
+            loadingbar.setTitle("Driver Login");
+            loadingbar.setMessage("Please wait  ...");
+            loadingbar.show();
+
+
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+
+                            if (task.isSuccessful())
+                            {
+                                Toast.makeText(DriverLogin.this,"Successful Login",Toast.LENGTH_LONG).show();
+                                loadingbar.dismiss();
+
+                                Intent x= new Intent(DriverLogin.this,DriverMap.class);
+                                        startActivity(x);
+
+                            }
+
+                            else
+                            {
+                                Toast.makeText(DriverLogin.this,"Sign up failed , please try again",Toast.LENGTH_LONG).show();
+                                loadingbar.dismiss();
+                            }
+
+                        }
+                    });
+
+
+        }
+
+
+    }
+
+    private  void Registerdriver(String email, String password)
+    {
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(DriverLogin.this,"Please enter email",Toast.LENGTH_LONG).show();
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(DriverLogin.this,"Please enter a valid password",Toast.LENGTH_LONG).show();
+        }
+        if(email.length() !=6 )
+        {
+            Toast.makeText(DriverLogin.this,"Password must be 6 digits",Toast.LENGTH_LONG).show();
+        }
+
+
+        else{
+
+            loadingbar.setTitle("Driver registration");
+            loadingbar.setMessage("Please wait while we register your data ...");
+            loadingbar.show();
+
+
+            auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+
+                            if (task.isSuccessful())
+                            {
+                                Toast.makeText(DriverLogin.this,"Succeful Signup",Toast.LENGTH_LONG).show();
+                                loadingbar.dismiss();
+
+
+                                Intent x= new Intent(DriverLogin.this,DriverMap.class);
+                                startActivity(x);
+
+                            }
+
+                            else
+                            {
+                                Toast.makeText(DriverLogin.this,"Sign up failed , please try again later",Toast.LENGTH_LONG).show();
+                                loadingbar.dismiss();
+                            }
+
+                        }
+                    });
+
+
+        }
+    }
+
 }
+
+
