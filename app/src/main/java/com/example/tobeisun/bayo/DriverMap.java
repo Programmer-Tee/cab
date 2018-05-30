423ad9daac3e965a480ffc062f558af5136dc106
t.obeisun <obeisuntabithaa@gmail.com>
2018-05-30 14:41:23 +0100

    no errors , driver and customer register and login, driver's map, tryin to get the current location

--- app/src/main/java/com/example/tobeisun/bayo/DriverMap.java
+++ app/src/main/java/com/example/tobeisun/bayo/DriverMap.java
@@ -1,8 +1,20 @@
 package com.example.tobeisun.bayo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,10 +22,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DriverMap extends FragmentActivity implements OnMapReadyCallback
-               {
+
+// permission was given for internet and coarse  and library was added in the gradle module app
+public class DriverMap extends FragmentActivity implements OnMapReadyCallback,
+
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private GoogleMap mMap;
+    GoogleApiClient googleApiClient;
+    Location lastlocation; //to track the last location
+    LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +43,74 @@ public class DriverMap extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+    @Override   //for getting the current location
+    public void onMapReady(GoogleMap googleMap) { //a boiler codewas here but it was removed and the following code was added to it{
         mMap = googleMap;
+        buildGoogleApiClient();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;// generated automatically from the code below
+        }
+          mMap.setMyLocationEnabled(true); // an error flags up ; then the yellow bulb is clicked and a permission check is enabled .. the code above is generated automatically with a list of comments and the comments were removed by me
+
+
+//setting the uer's location
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        locationRequest = new LocationRequest();
+        locationRequest.setFastestInterval(1000); // this is to update the location every second should incase the location changes
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return; // generated automatically by the code below; the comments were cleared by me so fear not
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);   // after this , add permission check by clicking on the yellow bulb beside it ; thats how the code was generated above
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    // when the driver changes location
+    @Override
+    public void onLocationChanged(Location location) {
+                               //  ^
+                               // /
+        lastlocation = location; //
+
+        //when the location of the driver changes ;
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+    }
+
+
+    protected synchronized void buildGoogleApiClient()
+    {
+        googleApiClient= new GoogleApiClient.Builder( this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        googleApiClient.connect();
+
     }
 }
