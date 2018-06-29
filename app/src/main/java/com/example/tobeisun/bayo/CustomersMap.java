8360a81836a1b8607d25f70c1e9672c4c1504f82
t.obeisun <obeisuntabithaa@gmail.com>
2018-06-29 17:22:38 +0100

    should launch the map first if already a user....

--- app/src/main/java/com/example/tobeisun/bayo/CustomersMap.java
+++ app/src/main/java/com/example/tobeisun/bayo/CustomersMap.java
@@ -203,7 +203,7 @@ public class CustomersMap extends AppCompatActivity
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "MyLocation button clicked", Toast             .LENGTH_SHORT).show();
 
         // Return false so that we
         // don't consume the event and the default behavior still occurs
---
+++ app/src/main/java/com/example/tobeisun/bayo/RequestTaxify.java
@@ -0,0 +1,13 @@
+package com.example.tobeisun.bayo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestTaxify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_taxify);
+    }
+}
